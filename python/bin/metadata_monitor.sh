c270202e6a7d90ab5936f4839fe96bac8f91591d
Ori Hoch <ori@uumpa.com>
2016-02-07 12:20:19 +0200

    allow metadata monitor bin to run from any directory

--- python/bin/metadata_monitor.sh
+++ python/bin/metadata_monitor.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
-# this script should run from knesset-data/python directory
-
 if [ "$1" == "" ] ; then
     echo "usage: $0 <type>"
     echo "<type> = initial or periodic"
 else
+    DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+    pushd $DIR/..  > /dev/null
     PYTHONPATH=. python knesset_data/dataservice/monitor/metadata/$1.py
+    popd  > /dev/null
 fi
