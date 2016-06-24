b2ff45382bf896cfc7d36f4e4f6c71e9a6abb666
Ori Hoch <ori@uumpa.com>
2016-06-24 10:03:30 +0300

    finalize the requests exception handling

--- python/knesset_data/dataservice/committees.py
+++ python/knesset_data/dataservice/committees.py
@@ -45,7 +45,7 @@ class Committee(BaseKnessetDataServiceCollectionObject):
 
 class CommitteeMeeting(BaseKnessetDataServiceFunctionObject):
     SERVICE_NAME = "committees"
-    METHOD_NAME = "CommitteeAgendaSearchx"
+    METHOD_NAME = "CommitteeAgendaSearch"
 
     # the primary key of committee meetings
     id = KnessetDataServiceSimpleField('Committee_Agenda_id')
