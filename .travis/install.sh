1202374e408d31a546a3de4f815f5a1ee8e3f6e1
Ori Hoch <ori@uumpa.com>
2017-02-07 22:47:49 +0200

    remove python from django requirements and travis

--- .travis/install.sh
+++ .travis/install.sh
@@ -4,7 +4,5 @@ set -e  # exit on errors
 
 pip install --upgrade pip
 pip install -r django/requirements.txt
-pip install -r python/requirements.txt
-pip install python/
 pip install django/
 pip install https://github.com/hasadna/Open-Knesset/archive/e28339da7ca92df96fc79b89351286e2715fcff0.zip#egg=Open-Knesset
--- .travis/script.sh
+++ .travis/script.sh
@@ -1,12 +1,6 @@
 #!/usr/bin/env bash
 
 set -e  # exit on errors
-
-pushd python > /dev/null
-    echo "Running python tests"
-    bin/run_tests.sh
-popd > /dev/null
-
 pushd django > /dev/null
     echo "Running django tests"
     ./manage.py test
